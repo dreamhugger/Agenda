4b336e52722be16297120e0152f5fb1d0f3fe43c
Marcus Mazzieri <124407140+dreamhugger@users.noreply.github.com>
2023-05-09 20:27:10 -0300

    Add files via upload

--- pj_agenda/pj_agenda/Cl_ControleContato.cs
+++ pj_agenda/pj_agenda/Cl_ControleContato.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace pj_agenda
 {
@@ -15,7 +16,7 @@ namespace pj_agenda
         {
             try
             {
-                string sql = "INSERT INTO tbcontato (nome, telefone, celular, email) " + "VALUES ('" + cont.Nome + "', '" + cont.Telefone + "', " + "'" + cont.Celular + "', '" + cont.Email + "')";
+                string sql = "INSERT INTO tb_contato (nome, telefone, celular, email) " + "VALUES ('" + cont.Nome + "', '" + cont.Telefone + "', " + "'" + cont.Celular + "', '" + cont.Email + "')";
 
                 MySqlCommand cmd = new MySqlCommand(sql, c.con);
 
@@ -36,7 +37,7 @@ namespace pj_agenda
         {
             try
             {
-                string sql = "update tbcontato set nome = '" + cont.Nome + "' , " + "telefone = '" + cont.Telefone + "', celular = '" + cont.Celular + "', email = '" + cont.Email + "' where codcontato = " + cont.Codcontato + " ; ";
+                string sql = "update tb_contato set nome = '" + cont.Nome + "' , " + "telefone = '" + cont.Telefone + "', celular = '" + cont.Celular + "', email = '" + cont.Email + "' where cod_contato = " + cont.Codcontato + " ; ";
 
                 MySqlCommand cmd = new MySqlCommand(sql, c.con);
 
@@ -76,7 +77,7 @@ namespace pj_agenda
 
             try
             {
-                string sql = "select * from tbcontato where codcontato= " + codigo + ";";
+                string sql = "select * from tb_contato where cod_contato= " + codigo + ";";
 
                 MySqlCommand cmd = new MySqlCommand(sql, c.con);
                 c.conectar();
@@ -89,7 +90,7 @@ namespace pj_agenda
                 else
                 {
                     objDados.Read();
-                    cont.Codcontato = Convert.ToInt32(objDados["codcontato"]);
+                    cont.Codcontato = Convert.ToInt32(objDados["cod_contato"]);
                     cont.Nome = objDados["nome"].ToString();
                     cont.Telefone = objDados["telefone"].ToString();
                     cont.Celular = objDados["celular"].ToString();
@@ -107,6 +108,90 @@ namespace pj_agenda
             {
                 c.desconectar();
             }
+        }
+
+        public DataTable pesquisaCodigo(int codigo)
+        {
+            string sql = "select cod_contato as 'Código', nome as Nome, telefone as Telefone, " +
+                "celular as Celular, email as Email from tb_contato where cod_contato = " + codigo + " ; ";
+            MySqlCommand cmd = new MySqlCommand(sql, c.con);
+            c.conectar();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable contato = new DataTable();
+            da.Fill(contato);
+            c.desconectar();
+            return contato;
+        }
+
+        public DataTable pesquisaNome(string nomecontato)
+        {
+            string sql = "select cod_contato as 'Código', nome as Nome, telefone as Telefone, " +
+                "celular as Celular, email as Email from tb_contato where nome like '%" + nomecontato + "%'";
+            MySqlCommand cmd = new MySqlCommand(sql, c.con);
+            c.conectar();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable contato = new DataTable();
+            da.Fill(contato);
+            c.desconectar();
+            return contato;
+        }
+
+        public DataTable pesquisaTelefone(string telefone)
+        {
+            string sql = "select cod_contato as 'Código', nome as Nome, telefone as Telefone, " +
+                "celular as Celular, email as Email from tb_contato where nome like '%" + telefone + "%'";
+            MySqlCommand cmd = new MySqlCommand(sql, c.con);
+            c.conectar();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable contato = new DataTable();
+            da.Fill(contato);
+            c.desconectar();
+            return contato;
+        }
+
+        public DataTable pesquisaCelular(string celular)
+        {
+            string sql = "select cod_contato as 'Código', nome as Nome, telefone as Telefone, " +
+                "celular as Celular, email as Email from tb_contato where nome like '%" + celular + "%'";
+            MySqlCommand cmd = new MySqlCommand(sql, c.con);
+            c.conectar();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable contato = new DataTable();
+            da.Fill(contato);
+            c.desconectar();
+            return contato;
+        }
+
+        public DataTable pesquisaEmail(string email)
+        {
+            string sql = "select cod_contato as 'Código', nome as Nome, telefone as Telefone, " +
+                "celular as Celular, email as Email from tb_contato where nome like '%" + email + "%'";
+            MySqlCommand cmd = new MySqlCommand(sql, c.con);
+            c.conectar();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable contato = new DataTable();
+            da.Fill(contato);
+            c.desconectar();
+            return contato;
+        }
+
+        public DataTable PreencherTodos()
+        {
+            string sql = "select cod_contato as 'Código', nome as Nome, telefone as Telefone, " +
+                "celular as Celular, email as Email from tb_contato ; ";
+            MySqlCommand cmd = new MySqlCommand(sql, c.con);
+            c.conectar();
+
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            DataTable contato = new DataTable();
+            da.Fill(contato);
+            c.desconectar();
+            return contato;
         }
     }
 }
---
+++ pj_agenda/pj_agenda/Frm_Principal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pj_agenda
+{
+    public partial class Frm_Principal : Form
+    {
+        public Frm_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Frm_Login TelaLogin = new Frm_Login();
+            TelaLogin.ShowDialog();
+            Cl_Conexao conexao = new Cl_Conexao();
+            MessageBox.Show(conexao.conectar());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToString();
+        }
+
+        private void cadastrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Cadastro cadastro = new Frm_Cadastro();
+            cadastro.MdiParent = this;
+            cadastro.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Sobre sobre = new Frm_Sobre();
+            sobre.ShowDialog();
+        }
+
+        private void consultaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Consulta consulta = new Frm_Consulta();
+            consulta.ShowDialog();
+        }
+    }
+}
